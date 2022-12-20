8234c52b009066ae1e93be17d53b56d445460b3d
Sheenor101 <115580397+Sheenor101@users.noreply.github.com>
2022-12-20 17:58:09 +0000

    Add files via upload

---
+++ Data1.R
@@ -0,0 +1,8 @@
+dataset <- read.csv ("D:\\DKIT\\Data Analytics\\dataset.csv")
+
+dataset
+
+log_scale = log(as.data.frame(dataset[6])) #normalising the data using logarithmic normalisation
+
+log_scale
+
