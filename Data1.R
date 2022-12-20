39198d632b89121adb026ce564cb380cf0503e48
Sheenor101 <115580397+Sheenor101@users.noreply.github.com>
2022-12-20 17:58:40 +0000

    Delete Data1.R

--- Data1.R
+++
@@ -1,8 +0,0 @@
-dataset <- read.csv ("D:\\DKIT\\Data Analytics\\dataset.csv")
-
-dataset
-
-log_scale = log(as.data.frame(dataset[6])) #normalising the data using logarithmic normalisation
-
-log_scale
-
