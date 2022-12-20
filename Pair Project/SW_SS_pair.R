c72272d06c6d5cacde6bb57afe132c8097b92483
Silvia Sacchi <silvi.sak93@gmail.com>
2022-12-20 19:00:59 +0000

    First commit

---
+++ Pair Project/SW_SS_pair.R
@@ -0,0 +1,7 @@
+################################################################################
+#############################   CA3   ##########################################
+################################################################################
+###OPENING CSV FILE
+
+dataset <- read.csv ("C:\\Users\\Silvia\\OneDrive - Dundalk Institute of Technology\\University\\Data Analytics for Immersive Environments\\CA3\\daie_ca3_data_6.csv")
+dataset
